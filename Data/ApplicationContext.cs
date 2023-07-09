e2c536f8b87fa31012875c9ad3eb176c88f40dc7
Davi Holanda <daviht7@gmail.com>
2023-07-08 23:15:18 -0300

    Criando classes de entidades

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,23 @@
+using System;
+using Curso.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Curso.Data
+{
+  public class ApplicationContext : DbContext
+  {
+    public DbSet<Departamento> Departamentos { get; set; }
+    public DbSet<Funcionario> Funcionarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      const string strConnection = "Data source=LAPTOP-A5UTA3O1\\SQLEXPRESS01;Initial Catalog=C002;Integrated Security=true;pooling=true";
+      optionsBuilder
+      .UseSqlServer(strConnection)
+      .EnableSensitiveDataLogging()
+          .LogTo(Console.WriteLine, LogLevel.Information);
+    }
+
+  }
+}
